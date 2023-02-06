7dcfd1fd1c8b4f1c743abb91cf6665d172db747a
Ramanpreet <rsingh639@myseneca.ca>
2023-02-05 20:27:39 -0500

    initial commit

---
+++ blog-service.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+var categories = [];
+var posts = [];
+exports.initialize = () =>
+{
+    return new Promise((resolve,reject)=>{
+fs.readFile('./data/posts.json', (err, data) => {
+    if (err) {
+      reject("Error while reading the file");
+      return;
+    }
+    posts = JSON.parse(data);
+    
+});
+
+fs.readFile('./data/categories.json', (err, data) => {
+    if (err) {
+      reject("Error while reading the file");
+      return;
+    }
+    categories = JSON.parse(data);
+})
+resolve;
+})
+};
+
+exports.getAllPosts = () =>
+{
+    return new Promise((resolve,reject)=>{
+        if (posts.length == 0)
+        {
+            reject('no posts yet');
+        }
+        resolve(posts);
+    })
+}
+
+exports.getPublishedPosts = () =>
+{
+    return new Promise((resolve,reject) =>
+    {
+        var publish = posts.filter(post => post.published == true);
+        if (publish.length == 0)
+        {
+            reject('no results yet');
+        }
+        resolve(publish);
+    })
+}
+
+
+
+exports.getCategories = () =>
+{
+    return new Promise((resolve,reject)=>{
+        if (categories.length == 0)
+        {
+            reject('no categories yet');
+        }
+        resolve(categories);
+    })
+}
--- server.js
+++ server.js
@@ -1,26 +1,72 @@
 var express = require("express");
+
 var app = express();
+
 app.use(express.static('public'));
 var HTTP_PORT = process.env.PORT || 8080;
-const path = require("path")
+const path = require("path");
+var blogservice = require(__dirname + "/blog-service.js");
+
+
+
+
+
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
 
-// app.get("/", function(req,res){
-//    // res.send("Hello World);
-// });
 
-
-app.get("/", function(req,res){
+app.get("/", function(_req,res){
     // res.send("<h3>About</h3>");
     
     res.redirect('/about');
 });
-app.get("/about",(req,res)=>
+app.get("/about",(_req,res)=>
 {
   res.sendFile(path.join(__dirname,'/views/about.html'))
 });
-app.listen(HTTP_PORT, onHttpStart);
+
+
+
+app.get("/blog", function (_req, res) {
+  blogservice.getPublishedPosts().then((data) => {
+    res.json({data});
+}).catch((err) => {
+    res.json({message: err});
+})
+});
+
+app.get("/posts", function (_req, res) {
+  blogservice.getAllPosts().then((data) => {
+    res.json({data});
+}).catch((err) => {
+    res.json({message: err});
+})
+});
+
+app.get("/categories", function (_req, res) {
+  blogservice.getCategories().then((data) => {
+    res.json({data});
+}).catch((err) => {
+    res.json({message: err});
+})
+});
+
+
+
+// app.get("/about", function(req,res){
+//   res.send('TODO: return')
+//  });
+
+app.use((req, res) => {
+  res.status(404).end('404 PAGE NOT FOUND');
+});
+
+blogservice.initialize().then(() => {
+  app.listen(HTTP_PORT, onHttpStart());
+}).catch (() => {
+  console.log('promises unfulfilled');
+});
+  
